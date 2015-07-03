1af0f07d44e7d3520522a7156ee05d2e5b681e61
Tim Ropchan <tropchan@alumni.uwo.ca>
2015-07-03 11:17:21 -0400

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'About' => 'pages#About'
+  get 'about' => 'pages#about'
+  get 'dashboard' => 'pages#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
